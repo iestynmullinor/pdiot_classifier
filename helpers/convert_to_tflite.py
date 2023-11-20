befe1b1ac83de977a341e6c04a952a6cd6b377ee
Nickon Tajali <s2063346@ed.ac.uk>
2023-11-20 20:42:15 +0000

    Tflite model without FFT

--- helpers/convert_to_tflite.py
+++ helpers/convert_to_tflite.py
@@ -7,13 +7,13 @@ ORIGINAL_MODEL = NAME_OF_MODEL_TO_CONVERT + ".keras"
 NAME_OF_TARGET = NAME_OF_MODEL_TO_CONVERT + ".tflite"
 
 
-model = load_model("models/presentation_models/" + ORIGINAL_MODEL)
-tf.saved_model.save(model, "saved_model_keras_dir")
+model = load_model("../models/presentation_models/" + ORIGINAL_MODEL)
+tf.saved_model.save(model, "../models/presentation_models/")
 
 # Convert the model
 converter = tf.lite.TFLiteConverter.from_saved_model("saved_model_keras_dir") # path to the SavedModel directory
 tflite_model = converter.convert()
 
 # Save the model.
-with open("models/presentation_models/" + NAME_OF_TARGET, 'wb') as f:
+with open("../models/presentation_models/" + NAME_OF_TARGET, 'wb') as f:
   f.write(tflite_model)
